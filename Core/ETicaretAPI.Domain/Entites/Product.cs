38f4bb0acf9bf30ca23fec2e958fbc695fe6ede4
İrfan Yunus Soydan <irfanysoydan@gmail.com>
2023-06-13 18:00:54 +0300

    Add project files.

---
+++ Core/ETicaretAPI.Domain/Entites/Product.cs
@@ -0,0 +1,18 @@
+﻿using ETicaretAPI.Domain.Entites.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Domain.Entites
+{
+    public class Product : BaseEntity
+    {
+        public string Name { get; set; }
+
+        public int Stock { get; set; }
+
+        public long Price { get; set; } 
+    }
+}
---
+++ Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using ETicaretAPI.Application.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETicaretAPI.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            var products = _productService.GetProducts();
+            return Ok(products);
+        }
+    }
+}
